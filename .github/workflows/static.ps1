e281d4db17e13f5d9665fb48ee5af5c59971520e
Couleur <82747632+couleurm@users.noreply.github.com>
2024-02-01 16:25:32 +0100

    Update static.ps1

--- .github/workflows/static.ps1
+++ .github/workflows/static.ps1
@@ -19,6 +19,19 @@ Remove-Item ./out/, ./$pg.zip, ./$pg/, file_id.diz -Recurse -Force
 
 Get-ChildItem *.html | ForEach-Object {
     $content = Get-Content $PSItem.FullName
+
+    # remove large footer banner, the "made with $bd" clearly suffices --'
     $content = $content -replace [regex]::escape('<div class="jw-strip jw-strip--color-default jw-strip--padding-end" >'), '<div style="display:none">'
+
+    # $pg does not include background image, so the following injects it back into the css
+    $live = Invoke-RestMethod "$uri/$($PSItem.BaseName)"
+
+    $bg_line = $live -split "`n" | Where-Object {$_ -like "*background-image*"}
+
+    $background_image = ($bg_line | Select-String -Pattern 'background-image: url\(&#039;(.*?)&#039;\);' -AllMatches | ForEach-Object { $_.Matches }).Value
+
+    $content = $content -replace 'background-position', ($background_image + 'background-position')
+
+    # write the changes back
     Set-Content -Path $PSItem.FullName -Value $content
 }
